249315e603b8caac95af11111aa87ef803945a7b
Edson Macamo <edsonmacamo85@gmail.com>
2022-04-14 12:48:08 +0200

    Navegando entre paginas

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,7 @@
+import Link  from 'next/link'
 export default function About(){
-    return <h1>Pagina de About</h1>
+    return  <>
+    <h1>Pagina de About</h1>
+    <Link href="/"><a>Voltar</a></Link>
+    </>
 }
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,4 +1,11 @@
+import Link  from 'next/link'
 export default function Produtos(){
 
-    return <h1>Pagina de Productos</h1>
+    return (
+    <>
+    <h1>Pagina de Productos</h1>
+    <Link href="/"><a>Voltar</a></Link>
+    </>
+    )
+
 }
